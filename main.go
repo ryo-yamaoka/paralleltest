0911843ae0f5796e52619c08bb71ab2b118321e1
ryo-yamaoka <20574684+ryo-yamaoka@users.noreply.github.com>
2022-12-30 18:26:43 +0900

    Fix #5

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"golang.org/x/tools/go/analysis/singlechecker"
 
-	"github.com/kunwardeep/paralleltest/pkg/paralleltest"
+	"github.com/ryo-yamaoka/paralleltest/pkg/paralleltest"
 )
 
 func main() {
